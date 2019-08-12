d78292c71683aa355427ebca7dd66d2aa6c4dc34
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-08-12 14:14:33 +0200

    lazy comment

--- src/main/java/com/master/spring/spring1proj1/main/SortAlgorithm.java
+++
@@ -1,6 +0,0 @@
-package com.master.spring.spring1proj1.main;
-
-public interface SortAlgorithm {
-	
-	int[] sort(int[] numbers);
-}
--- src/main/java/com/master/spring/spring1proj1/main/Spring1proj1Application.java
+++ src/main/java/com/master/spring/spring1proj1/main/Spring1proj1Application.java
@@ -3,15 +3,23 @@ package com.master.spring.spring1proj1.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.master.spring.spring1proj1.res.BinarySearchImpl;
+
+/*
+ * This annotation @ComponentScan has an attribute 'basePackages'
+ * that takes array of strings representing packages to scan
+ */
+@ComponentScan(basePackages = { "com.master.spring.spring1proj1.res" })
 @SpringBootApplication
 public class Spring1proj1Application {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(Spring1proj1Application.class, args);
 		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
-		
-		int result = binarySearchImpl.binarySearch(new int[] {1, 2, 3}, 2);
+
+		int result = binarySearchImpl.binarySearch(new int[] { 1, 2, 3 }, 2);
 		System.out.println(result);
 	}
 
