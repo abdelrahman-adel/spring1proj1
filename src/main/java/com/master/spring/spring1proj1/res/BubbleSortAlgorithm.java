4dc2b00400ae05517916fcab21097cca230f34c8
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-08-12 16:28:53 +0200

    lazy comment

---
+++ src/main/java/com/master/spring/spring1proj1/res/BubbleSortAlgorithm.java
@@ -0,0 +1,12 @@
+package com.master.spring.spring1proj1.res;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BubbleSortAlgorithm implements SortAlgorithm {
+
+	@Override
+	public int[] sort(int[] numbers) {
+		return numbers;
+	}
+}
---
+++ src/main/java/com/master/spring/spring1proj1/res/SortAlgorithm.java
@@ -0,0 +1,6 @@
+package com.master.spring.spring1proj1.res;
+
+public interface SortAlgorithm {
+	
+	int[] sort(int[] numbers);
+}
